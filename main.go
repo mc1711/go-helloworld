2f90dc8c7768d58c1f25092e072894764bd30bc0
mcolu <mcolu@juniper.net>
2018-02-04 12:15:47 +0000

    package main
    import "net/http"
    import "fmt"
    import "os"
    import "io"
    
    type logWriter struct{}
    
    func main() {
    	resp, err := http.Get("http://google.com")
    	if err != nil {
    		fmt.Println("Error", err)
    		os.Exit(1)
    	}
    	lw := logWriter{}
    	io.Copy(lw, resp.Body)
    	// bs := make([]byte, 99999)
    	// resp.Body.Read(bs)
    	// fmt.Println(string(bs))
    }
    
    func (logWriter) Write(bs []byte) (int, error) {
    	fmt.Println("bla bla bla", string(bs))
    	return len(bs), nil
    }

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "net/http"
+import "fmt"
+import "os"
+import "io"
+
+type logWriter struct{}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println("bla bla bla", string(bs))
+	return len(bs), nil
+}
